cf05f786840fa5646ff427b415ae9f0a9624d758
jampool <jampoolgarcia@gmail.com>
2020-12-19 12:30:11 -0400

    create project, add dataSource and add user model, repository, controller

---
+++ src/models/user.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  userName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'number',
+    default: 1
+  })
+  role: number;
+
+  @property({
+    type: 'boolean',
+    default: false
+  })
+  isActive: boolean;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
